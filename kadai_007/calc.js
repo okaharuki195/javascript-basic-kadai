a76168fd7cb8b925804a28c8f3ef0ae59ea3eb2b
okaharuki195 <141206137+okaharuki195@users.noreply.github.com>
2023-10-16 12:03:12 +0900

    ファイルを格納した

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -12,7 +12,11 @@ let num = Math.floor(Math.random()* 100);
 //変数numの値を出力
 console.log(num);
 
-if(num%3===0){
+if(num%3===0 && num%5===0){
+    console.log('3と5の倍数です');
+}
+
+else if(num%3===0){
     console.log('3の倍数です');
 }
 
@@ -20,13 +24,21 @@ else if(num%5===0){
     console.log('5の倍数です');
 }
 
-else if(num%3===0 && num%5===0){
-    console.log('3と5の倍数です');
-}
-
 else{
     console.log(num);
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
